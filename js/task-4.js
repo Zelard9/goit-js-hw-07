90674ecd6fe4baa75756723751258ef5d858deea
Zelard9 <91178042+Zelard9@users.noreply.github.com>
2024-05-02 21:12:32 +0300

    Update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
+const formSend = document.querySelector(".login-form");
 
+formSend.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = this.elements.email.value.trim();
+  const password = this.elements.password.value.trim();
+
+  if (!email || !password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  this.reset();
+});
