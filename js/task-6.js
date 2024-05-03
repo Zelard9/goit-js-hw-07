8e145b99691e40cf8138710fe83e9ec575f36abc
Zelard9 <91178042+Zelard9@users.noreply.github.com>
2024-05-03 19:22:53 +0300

    update task-5,task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxContainer = document.querySelector("#boxes");
+function boxesCreate(amount) {
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const box = document.createElement(`div`);
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxContainer.append(box);
+  }
+}
+function boxesDestroy() {
+  boxContainer.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = input.value;
+  if (amount < 1 || amount > 100) {
+    return;
+  }
+  boxesCreate(amount);
+  input.value = "";
+});
+destroyButton.addEventListener("click", boxesDestroy);
